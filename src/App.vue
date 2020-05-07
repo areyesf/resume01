f3d2fe52ba2996a878ae26cb8965ea621ce39fef
Andres R - Notebook <andrereyes.xx@gmail.com>
2020-05-07 16:38:01 -0400

    agregado los componentes del header + responsividad

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <header>
       <!-- INFO -->
-      <div class="info trapecio">
+      <div class="info">
         <!-- NAVEGATION -->
         <div class="nav">
           <router-link to="/">Inicio</router-link>|
@@ -11,42 +11,44 @@
           <router-link to="/contacto">Contacto</router-link>
         </div>
         <!-- PERSONAL INFORMATION -->
-        <h1>Andrés Reyes</h1>
-        <ul>
-          <li>
-            <i class="far fa-envelope"></i>
-            an.reyesf@gmail.com
-          </li>
-          <li>
-            <i class="fab fa-whatsapp"></i>
-            +56 9 20 63 0111
-          </li>
-          <li>
-            <i class="fas fa-map-marker-alt"></i>
-            El Estribo, Melipilla, Región Metropolitana, CL
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <a href="#">
-              <i class="fab fa-linkedin"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fab fa-instagram"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fab fa-facebook-square"></i>
-            </a>
-          </li>
-        </ul>
+        <div class="personal-info">
+          <h1>Andrés Reyes</h1>
+          <ul class="info-contact">
+            <li>
+              <i class="far fa-envelope"></i>
+              an.reyesf@gmail.com
+            </li>
+            <li>
+              <i class="fab fa-whatsapp"></i>
+              +56 9 20 63 0111
+            </li>
+            <li>
+              <i class="fas fa-map-marker-alt"></i>
+              El Estribo, Melipilla, Región Metropolitana, CL
+            </li>
+          </ul>
+          <ul class="social-info">
+            <li>
+              <a href="#">
+                <i class="fab fa-linkedin"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fab fa-facebook-square"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       <!-- IMG PROFILE -->
       <div class="img-profile">
-        <img src="https://i.ibb.co/2Zdm46K/model-2911330-1280.jpg" alt="">
+        <img src="https://i.ibb.co/2Zdm46K/model-2911330-1280.jpg" alt />
       </div>
     </header>
     <!-- ROUTER VIEW -->
@@ -71,37 +73,97 @@ body {
 a {
   text-decoration: none;
 }
+ul {
+  list-style: none;
+}
 
 // COLOR PALETTE
 $cian: #60b8b8;
 $white: #fff;
+$gray: #6b6b6b;
+//break point to responsive design
+$break-point: 950px;
 
 header {
-  height: 30vh;
+  height: 334px;
   display: flex;
-  justify-content: space-between;
-
+  position: relative;
+  background-color: $cian;
   .info {
-    background-color: $cian;
     color: $white;
     height: 100%;
-    width: 100%;
-    border-right: 30vh solid red;
-    border-bottom: 30vh solid $cian;
+    width: 50%;
     .nav {
+      text-align: center;
+      padding: 1rem 0;
       a {
         color: $white;
+        padding: 0 10px;
+        transition: all ease .5s;
 
         &.router-link-exact-active {
-          color: #42b983;
+          border-bottom: 1px solid $white;
+          opacity: .5;
+        }
+      }
+    }
+    .personal-info {
+      padding: 0 2rem;
+      h1 {
+        padding: 1rem 0;
+      }
+      .info-contact {
+        li {
+          padding: 0.5rem 0;
+          i {
+            padding-right: 1rem;
+          }
+        }
+      }
+      .social-info {
+        width: 50%;
+        margin-top: 1rem;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        li {
+          a {
+            color: $white;
+            i {
+              font-size: 2rem;
+            }
+          }
         }
       }
     }
   }
   .img-profile {
-    width: 300px;
-    img{
+    width: 500px;
+    position: absolute;
+    right: 0;
+    img {
       width: 100%;
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 100%);
+    }
+  }
+
+  //MEDIA RULES
+  @media (max-width: $break-point) {
+    .info{
+      width: 100%;
+      .personal-info{
+        padding: 0 .5rem;
+        h1{
+          text-align: center;
+        }
+        .social-info{
+          padding-top: .5rem;
+          width: 100%;
+        }
+      }
+    }
+    .img-profile {
+      display: none;
     }
   }
 }
