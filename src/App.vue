6c5f42a84a01edbec454bc053f41ff7f1821ff1e
Andres R - Notebook <andrereyes.xx@gmail.com>
2020-05-07 15:11:15 -0400

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,107 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      <!-- INFO -->
+      <div class="info trapecio">
+        <!-- NAVEGATION -->
+        <div class="nav">
+          <router-link to="/">Inicio</router-link>|
+          <router-link to="/experiencia">Experiencia</router-link>|
+          <router-link to="/educacion">Educación</router-link>|
+          <router-link to="/contacto">Contacto</router-link>
+        </div>
+        <!-- PERSONAL INFORMATION -->
+        <h1>Andrés Reyes</h1>
+        <ul>
+          <li>
+            <i class="far fa-envelope"></i>
+            an.reyesf@gmail.com
+          </li>
+          <li>
+            <i class="fab fa-whatsapp"></i>
+            +56 9 20 63 0111
+          </li>
+          <li>
+            <i class="fas fa-map-marker-alt"></i>
+            El Estribo, Melipilla, Región Metropolitana, CL
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- IMG PROFILE -->
+      <div class="img-profile">
+        <img src="https://i.ibb.co/2Zdm46K/model-2911330-1280.jpg" alt="">
+      </div>
+    </header>
+    <!-- ROUTER VIEW -->
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+// IMPORT FONT
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap");
+
+// RESET
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+// GENERAL STYLE
+body {
+  font-family: "Roboto", sans-serif;
+}
+a {
+  text-decoration: none;
 }
 
-#nav {
-  padding: 30px;
+// COLOR PALETTE
+$cian: #60b8b8;
+$white: #fff;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+header {
+  height: 30vh;
+  display: flex;
+  justify-content: space-between;
 
-    &.router-link-exact-active {
-      color: #42b983;
+  .info {
+    background-color: $cian;
+    color: $white;
+    height: 100%;
+    width: 100%;
+    border-right: 30vh solid red;
+    border-bottom: 30vh solid $cian;
+    .nav {
+      a {
+        color: $white;
+
+        &.router-link-exact-active {
+          color: #42b983;
+        }
+      }
+    }
+  }
+  .img-profile {
+    width: 300px;
+    img{
+      width: 100%;
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Inicio",
+    component: () => import("../views/Inicio.vue"),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/experiencia",
+    name: "Experiencia",
+    component: () => import("../views/Experiencia.vue"),
+  },
+  {
+    path: "/educacion",
+    name: "Educacion",
+    component: () => import("../views/Educacion.vue"),
+  },
+  {
+    path: "/contacto",
+    name: "Contacto",
+    component: () => import("../views/Contacto.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
